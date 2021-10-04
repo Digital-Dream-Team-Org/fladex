c8933322f171fc5d11f5c5d545c97dce4f7c5dda
Vlad Starostin <w1df33r@gmail.com>
2021-10-04 08:09:48 +0700

    Add header layout Add footer layout Add home page sections layout Add icon fonts Add image assets Add fonts

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,4 @@
 (function ($) {
   // Document ready
-  $(function () {
-    console.log("ready");
-    $(".main-content .container").prepend("<p>JS works!</p>");
-  });
+  $(function () {});
 })(jQuery);
